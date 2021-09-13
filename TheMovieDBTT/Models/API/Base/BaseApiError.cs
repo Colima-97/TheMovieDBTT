b9ccca578a9aeebec1f4547e465476c21c336c01
Brandon Sandoval <colima_97@hotmail.com>
2021-09-12 23:39:02 -0500

    [WIP] - Create base api models

---
+++ TheMovieDBTT/Models/API/Base/BaseApiError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TheMovieDBTT.Models.API.Base
+{
+	public class BaseApiError
+	{
+		[JsonProperty("status_message")]
+		public string StatusMessage { get; set; }
+
+		[JsonProperty("status_code")]
+		public int StatusCode { get; set; }
+	}
+}
